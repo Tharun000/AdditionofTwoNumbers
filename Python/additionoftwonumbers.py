cced8f880326bf04e87cec2d02a14434a2041479
Charapalli-Tharun <56440485+Tharun000@users.noreply.github.com>
2020-10-01 17:33:41 +0530

    Update additionoftwonumbers.py

--- Python/additionoftwonumbers.py
+++ Python/additionoftwonumbers.py
@@ -5,3 +5,8 @@ num2 = 6.5
 sum = float(num1) + float(num2)
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+#Some what easy to implement
+#n1,n2=list(float(x) for x in input().split())
+#print(n1+n2)
